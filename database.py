32adad00a8858e2cfa758e95ef1a72cb9d9fb69a
Bruno Kemel <br.kemel@gmail.com>
2025-09-14 01:20:23 -0300

    readme

--- database.py
+++ database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-#start " uvicorn main:app --reload " api
 #banco SQL
 
 DATABASE_URL = "sqlite:///./users.db"
