b68aa59a71849da477fb5e1c8163aadf07c4b497
Bruno Kemel <br.kemel@gmail.com>
2025-09-26 20:38:28 -0300

    MySQL + ENV para arquivos com senhas

--- database.py
+++ database.py
@@ -1,12 +1,28 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+# Carrega as variáveis do arquivo .env (no seu caso credencias.env)
+load_dotenv("credenciais.env")
+
+# Pega os valores das variáveis
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
 
 #banco SQL
 
-DATABASE_URL = "sqlite:///./users.db"
+# DATABASE_URL = "sqlite:///./users.db"
+# ⚠️ Ajusta aqui com seu usuário, senha e nome do banco
+SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=engine)
 
 Base = declarative_base()
+
--- models.py
+++ models.py
@@ -5,5 +5,5 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    nome = Column(String, index=True)
-    email = Column(String, unique=True, index=True)
+    nome = Column(String(100), index=True)
+    email = Column(String(100), unique=True, index=True)
